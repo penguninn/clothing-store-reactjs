8b1fb6a583a41b8f92a8fecb463016324428da91
penguninn <coolermaster1082005@gmail.com>
2025-10-23 22:19:09 +0700

    init admin dashboard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "lucide-react";
 import type {ProductType} from "@/types";
-
 interface ProductCardProps {
   product: ProductType;
 }
